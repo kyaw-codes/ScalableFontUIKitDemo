39556249007612b53d3dc6314e39818e4c94073e
Kyaw Zay Ya Lin Tun <kyaw.codes@gmail.com>
2022-11-06 18:16:34 +0630

    chore: ui polish

--- ScalableFontDemo/ViewController.swift
+++ ScalableFontDemo/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var btnOrderNow: UIButton!
     
+    private let descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
     var descriptionStackView: UIStackView {
         priceLabel.superview as! UIStackView
     }
@@ -35,6 +37,7 @@ class ViewController: UIViewController {
         
         // configure descriptionTitleLabel
         descriptionTitleLabel.font = .systemFont(ofSize: 20)
+        descriptionLabel.text = descriptionText
 
         // configure descriptionLabel
         descriptionLabel.font = .systemFont(ofSize: 17)
@@ -44,7 +47,7 @@ class ViewController: UIViewController {
 
         // configure stackview spacing
         descriptionStackView.setCustomSpacing(12, after: priceLabel)
-        descriptionStackView.setCustomSpacing(4, after: descriptionTitleLabel)
+        descriptionStackView.setCustomSpacing(8, after: descriptionTitleLabel)
     }
 
 }
